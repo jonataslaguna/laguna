42917d2a80a0ae531115cba7716fa44d3a804728
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-09-14 13:19:04 -0300

    add a footer to the tech and projects page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,27 +1,35 @@
 import Card from "../../components/Card";
+import Footer from "../../components/Footer";
 import styles from "./projects.module.css"
 
 function Projects() {
+  const route = window.location.pathname;
   return (
-    <div className={ styles.projectsContainer }>
-  
-      <div className={ styles.projectsHeader }>
-        <h2>Projects</h2>
-        <h3>Aqui estão alguns dos meus projetos</h3>
-      </div>
+    <>
+      <div className={ styles.projectsContainer }>
+    
+        <div className={ styles.projectsHeader }>
+          <h2>Projects</h2>
+          <h3>Aqui estão alguns dos meus projetos</h3>
+        </div>
 
-      <div className={ styles.cards }>
-      <Card 
-        img="https://via.placeholder.com/150"
-        projectName="Recipes App"
-        description="Mobile recipe app"
-        technologies="Typescript, React, JavaScript, React Testing Library, API, CSS"
-        livePreviewLink="https://recipes-app-xz1r.vercel.app"
-        viewCodeLInk="https://github.com/jonataslaguna/recipes-app"
-       />
-
+        <div className={ styles.cards }>
+        <Card 
+          img="https://via.placeholder.com/150"
+          projectName="Recipes App"
+          description="Mobile recipe app"
+          technologies="Typescript, React, JavaScript, React Testing Library, API, CSS"
+          livePreviewLink="https://recipes-app-xz1r.vercel.app"
+          viewCodeLInk="https://github.com/jonataslaguna/recipes-app"
+        />
+        </div>
       </div>
-    </div>
+        { route === "/projects" && 
+         <div className={ styles.footer }>
+           <Footer />
+         </div>
+        }
+    </>
   );
 }
 
--- src/pages/Tech/index.tsx
+++ src/pages/Tech/index.tsx
@@ -1,62 +1,71 @@
+import Footer from '../../components/Footer';
 import styles from './tech.module.css';
 
 function Tech() {
+    const route = window.location.pathname;
     return (
-    <section className={ styles.container }>
-        <div>
-            <h2>My Tech Stack</h2>
-            <h3>Tecnologias que tenho conhecimento</h3>
-        </div>
-        <div>
-            <ul className={ styles.listTech }>
-                <div className={ styles.techsPart1 }>
-                    <li>
-                      <p className={ styles.name }>JavaScript</p>
-                      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt='JavaScript'/>
-                    </li>
-                    <li>
-                        <p className={ styles.name }>TypeScript</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" alt='TypeScript'/>
-                    </li>
-                    <li>
-                        <p className={ styles.name }>React</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='React'/>
-                    </li>
-                    <li> 
-                        <p className={ styles.name }>HTML 5</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt='HTML5' />
-                    </li>
-                    <li>
-                        <p className={ styles.name }>Css 3</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt='CSS3'/>
-                    </li>
+    <>
+      <section className={ styles.container }>
+            <div>
+                <h2>My Tech Stack</h2>
+                <h3>Tecnologias que tenho conhecimento</h3>
+            </div>
+            <div>
+                <ul className={ styles.listTech }>
+                    <div className={ styles.techsPart1 }>
+                        <li>
+                        <p className={ styles.name }>JavaScript</p>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt='JavaScript'/>
+                        </li>
+                        <li>
+                            <p className={ styles.name }>TypeScript</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" alt='TypeScript'/>
+                        </li>
+                        <li>
+                            <p className={ styles.name }>React</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='React'/>
+                        </li>
+                        <li> 
+                            <p className={ styles.name }>HTML 5</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt='HTML5' />
+                        </li>
+                        <li>
+                            <p className={ styles.name }>Css 3</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt='CSS3'/>
+                        </li>
+                    </div>
+                
+                    <div className={ styles.techsPart2 }>
+                        <li>
+                        <p className={ styles.name }>Redux</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt='Redux' />
+                        </li>
+                        <li> 
+                            <p className={ styles.name }>Jest</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"  alt='Jest'/>
+                        </li>
+                        <li>
+                            <p className={ styles.name }>Git</p>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Git"/>
+                        </li>
+                        <li>
+                        <p className={ styles.name }>GitHub</p>
+                            <img src="/images/gitHubLogo.png" alt="GitHub"/>
+                        </li>
+                        <li>
+                            <p className={ styles.name }>Testing Library</p>
+                            <img src="/svg/testing-library.svg" alt="React Testing Library"/>
+                        </li>
                 </div>
-            
-                <div className={ styles.techsPart2 }>
-                    <li>
-                       <p className={ styles.name }>Redux</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt='Redux' />
-                    </li>
-                    <li> 
-                        <p className={ styles.name }>Jest</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"  alt='Jest'/>
-                    </li>
-                    <li>
-                        <p className={ styles.name }>Git</p>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Git"/>
-                    </li>
-                    <li>
-                       <p className={ styles.name }>GitHub</p>
-                        <img src="/images/gitHubLogo.png" alt="GitHub"/>
-                    </li>
-                    <li>
-                        <p className={ styles.name }>Testing Library</p>
-                        <img src="/svg/testing-library.svg" alt="React Testing Library"/>
-                    </li>
-               </div>
-            </ul>
+                </ul>
+            </div>
+      </section>
+      { route === "/techStack" && 
+        <div className={ styles.footer }>
+          <Footer />
         </div>
-    </section>
+       }
+    </>
     )
 }
 
