577fcc55786748ac5244d87206cad71189dfb748
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-09-25 07:05:52 -0300

    add my image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ function Home() {
         <h3>Front-end Developer</h3>
         <div className={ styles.myImageDivMobile }>
         <span className={ styles.circleSpinMobile }></span>
-        <img src="/images/profileIcon.png" alt="image exemple" /> 
+        <img src="/images/jonatas-l.jpg" alt="jonatas laguna" />
         </div>
         <p>Olá, seja bem vindo ao meu portfólio pessoal, aqui você pode encontrar alguns dos meus projetos e entrar em contato comigo.</p>
         <div className={ styles.btnBox }>
@@ -22,7 +22,7 @@ function Home() {
         </div>
       </div>
       <div className={ styles.myImageDiv }>
-       <img src="/images/profileIcon.png" alt="image exemple" />
+       <img src="/images/jonatas-l.jpg" alt="jonatas laguna" />
        <span className={ styles.circleSpin }></span>
       </div>
     </section>
