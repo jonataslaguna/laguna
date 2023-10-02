90877bde3dd3a30a1ec5229852587657034f21d1
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-02 10:31:11 -0300

    general CSS adjustments

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,19 +16,23 @@ function Home() {
         <span className={ styles.circleSpinMobile }></span>
         <img src="/images/jonatas-laguna.png" alt="jonatas laguna" />
         </div>
-        <p>Olá, seja bem vindo ao meu portfólio pessoal, aqui você pode encontrar alguns dos meus projetos e entrar em contato comigo.</p>
-        <div className={ styles.btnBox }>
-          <Link to='/about' onClick={ () => handleClickRoutes('/about')}>About</Link>
-          <Link to='/projects' onClick={ () => handleClickRoutes('/projects')}>Projets</Link>
-        </div>
+          <p>
+            Olá, seja bem vindo ao meu portfólio pessoal, 
+            aqui você pode encontrar alguns dos meus projetos 
+            e entrar em contato comigo.
+          </p>
+          <div className={ styles.btnBox }>
+            <Link to='/about' onClick={ () => handleClickRoutes('/about')}>About</Link>
+            <Link to='/projects' onClick={ () => handleClickRoutes('/projects')}>Projets</Link>
+          </div>
         <div className={ styles.links }>
           <a href="https://github.com/jonataslaguna" target="_blank"><i className='bx bxl-github'></i></a>
           <a href="https://www.linkedin.com/in/jonataslaguna/" target="_blank"><i className='bx bxl-linkedin'></i></a>
         </div>
       </div>
       <div className={ styles.myImageDiv }>
-       <img src="/images/jonatas-laguna.png" alt="jonatas laguna" />
-       <span className={ styles.circleSpin }></span>
+        <img src="/images/jonatas-laguna.png" alt="jonatas laguna" />
+        <span className={ styles.circleSpin }></span>
       </div>
     </section>
   );
