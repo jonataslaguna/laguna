0eb78574af73d1e88e62727ea96a471f026e35cf
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-10-16 14:07:35 -0300

    add projects cards

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -13,14 +13,34 @@ function Projects() {
         </div>
 
         <div className={ styles.cards }>
-        <Card 
-          img="https://via.placeholder.com/150"
-          projectName="Recipes App"
-          description="⚠️ Leia o ReadMe antes de utilizar ⚠️"
-          technologies="Typescript, React, JavaScript, React Testing Library, API, CSS"
-          livePreviewLink="https://recipes-app-xz1r.vercel.app"
-          viewCodeLInk="https://github.com/jonataslaguna/recipes-app"
-        />
+
+          <Card 
+            img="https://cdn.mos.cms.futurecdn.net/rDJegQJaCyGaYysj2g5XWY.jpg"
+            projectName="Netflix Clone"
+            description="App de filmes e séries"
+            technologies="Typescript, React, Testing Library, API, ContextApi, CSS"
+            livePreviewLink="https://netflix-clone-flax-ten-98.vercel.app"
+            viewCodeLInk="https://github.com/jonataslaguna/netflix-clone"
+          />
+
+          <Card 
+            img="https://agenciadenoticias.ibge.gov.br/images/agenciadenoticias/ibge/agencia-home.jpg"
+            projectName="Trybe News"
+            description="App de notícias utilizando API do IBGE"
+            technologies="Typescript, React, Testing Library, API, ContextApi, CSS"
+            livePreviewLink="https://trybe-news-eight.vercel.app"
+            viewCodeLInk="https://github.com/jonataslaguna/trybe-news"
+          />
+
+          <Card 
+            img="/images/projects/recipes-app-logo.svg"
+            projectName="Recipes App"
+            description="⚠️ Leia o ReadMe antes de utilizar ⚠️"
+            technologies="Typescript, React, JavaScript, Testing Library, API, ContextApi, CSS"
+            livePreviewLink="https://recipes-app-xz1r.vercel.app"
+            viewCodeLInk="https://github.com/jonataslaguna/recipes-app"
+          />
+
         </div>
       </div>
         <div className={ styles.footer }>
